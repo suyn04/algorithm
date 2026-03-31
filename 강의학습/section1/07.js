fb1cbcad37c57e59428505080c73a9b77127361c
suyn04 <63181309+suyn04@users.noreply.github.com>
2026-03-31 17:26:33 +0900

    7. 10부제

---
+++ 강의학습/section1/07.js
@@ -0,0 +1,31 @@
+// 10부제
+
+// 기본 답변
+// function solution(day, arr) {
+//     let answer = 0;
+//     for (let x of arr) {
+//         if (x % 10 == day) answer++;
+//     }
+
+//     return answer;
+// }
+
+// 답변 2
+// filter 함수 이용
+// function solution(day, arr) {
+//     return arr.filter((v) => v % 10 === day).length;
+// }
+
+// 답변 3
+// reduce 사용
+function solution(day, arr) {
+    return arr.reduce((count, v) => {
+        return v % 10 === day ? count + 1 : count;
+    }, 0);
+}
+
+arr = [25, 23, 11, 47, 53, 17, 33];
+arr2 = [12, 20, 54, 30, 87, 91, 30];
+
+console.log(solution(3, arr));
+console.log(solution(0, arr2));
